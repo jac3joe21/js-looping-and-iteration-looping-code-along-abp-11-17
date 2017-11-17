48715f2524fa42073e5074c8607e4445b9596471
Joseph Casiano <josephcasiano@gmail.com>
2017-11-17 20:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
 const printBadges = (['Lisa', 'Kaitlin', 'Jan']);
 
-function printBadges () {
+function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #1`);
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}`);
   }
   return names;
 }
