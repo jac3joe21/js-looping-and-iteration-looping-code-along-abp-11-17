5cd45f9b44b092840799c97c20e432359bfb5d49
Joseph Casiano <josephcasiano@gmail.com>
2017-11-17 20:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ function printBadges (names) {
 }
 
 function tailsNeverFails () {
+   let tails = 0;
 
+   while (Math.random() >= 0.5;) {
+     tails++;
+   }
+     
+   return `You got ${tails} tails in a row!`;
 }
+
