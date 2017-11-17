544c64bb3f8c75fe9ba44ce392efb0c483fb7f88
Joseph Casiano <josephcasiano@gmail.com>
2017-11-17 20:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const printBadges = (['Ada', 'Brendan', 'Ali']);
 
 function printBadges () {
   for (let i = 0; i < names; i++) {
-    console.log(`Welcome `)
+    console.log(`Welcome ${printBadges[i]}! You are employee #1`);
   }
+  return printBadges;
 }
