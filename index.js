71492a6ea9dd447b0f38669e2c410e18ed6e146d
Joseph Casiano <josephcasiano@gmail.com>
2017-11-17 20:22:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const names = ['Ada', 'Brendan', 'Ali'];
+const names = ['Joe', 'Gabe'];
 
 function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
